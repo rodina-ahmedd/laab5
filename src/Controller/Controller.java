a5cd3b2639829bde40b06119f450ec2b1cd49969
abdelrahman <abdoelsayedmo2005@gmail.com>
2025-10-26 23:59:52 +0300

    editing the controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,10 +4,42 @@
  */
 package Controller;
 
-/**
- *
- * @author mo
- */
+import Backend.Student;
+import Backend.StudentDataBase;
+import java.io.IOException;
+import java.util.List;
+
 public class Controller {
+
+    private StudentDataBase Data;
+
+    public Controller(StudentDataBase Data) {
+        this.Data = Data;
+    }
+
+    public List<Student> getAllStudents() throws IOException {
+         return Data.loadStudentsFromFile();
+    }
+
+    public Student searchById(int id) {
+        return Data.getById(id);
+    }
+
+    public Student searchByName(String name) {
+        return Data.getByName(name);
+    }
+
+    public void addStudent(String name, int age, String gender, String department, double gpa) throws IOException {
+        int id = Data.generateId();
+        Student s = new Student(id, name, age, gender, name, gpa);
+        Data.addStudent(s);
+    }
+
+    public void updateStudent(int id, String name, int age, String gender, String department, double gpa) throws IOException {
+        Data.updateStudent(id, name, age, name, gpa);
+    }
     
+     public void deleteStudent(int id) throws IOException {
+        Data.DeleteStudent(id);
+    }
 }
