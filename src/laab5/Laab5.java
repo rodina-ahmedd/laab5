3707f28cd6787651e93f4d5f4754de41625afc14
abdelrahman <abdoelsayedmo2005@gmail.com>
2025-10-27 20:04:48 +0300

    the main class

--- src/laab5/Laab5.java
+++ src/laab5/Laab5.java
@@ -4,17 +4,32 @@
  */
 package laab5;
 
+import Backend.StudentDataBase;
+import Controller.Controller;
+import Frontend.Login;
+import java.io.IOException;
+
 /**
  *
  * @author mo
  */
 public class Laab5 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    private static Controller control ;
+    
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        StudentDataBase Data  = new StudentDataBase();
+      
+      control = new Controller(Data);
+      
+      new Login().setVisible(true);
+       
+    }
+    
+    public static Controller getController(){
+        return control;
     }
     
 }
